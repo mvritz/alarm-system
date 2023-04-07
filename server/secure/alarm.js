a3747af1dcfbab5cb3900ada3fe641d2b7dbf84a
andrija-vuksanovic-titanom <andrija.vuksanovic@titanom.com>
2023-04-07 18:43:04 +0200

    fix: adapt urls

--- server/secure/alarm.js
+++ server/secure/alarm.js
@@ -1,23 +1,24 @@
-const button = document.querySelector("#checkbox");
-const body = document.querySelector("body");
-const modal = document.querySelector(".modal");
+const button = document.querySelector('#checkbox');
+const body = document.querySelector('body');
+const modal = document.querySelector('.modal');
 
-button.addEventListener("change", (event) => {
+button.addEventListener('change', (event) => {
+  console.log(event);
   if (event.target.checked) {
-    body.classList.add("alarm");
-    modal.style.display = "initial";
+    body.classList.add('alarm');
+    modal.style.display = 'initial';
   } else {
-    body.classList.remove("alarm");
-    modal.style.display = "none";
+    body.classList.remove('alarm');
+    modal.style.display = 'none';
   }
 
   fetch(`/start-alarm`, {
-    method: "post",
+    method: 'post',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      alarm: e.target.checked,
+      alarm: event.target.checked,
     }),
   });
 });
