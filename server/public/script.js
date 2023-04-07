6d12c8f923a5f06b3347e832e42586be33501942
Moritz Dresch <moritz.dresch@titanom.com>
2023-04-07 18:20:43 +0200

    feat: alarm frontend

--- server/public/script.js
+++ server/public/script.js
@@ -1,10 +1,20 @@
-const button = document.getElementById('button');
+const button = document.querySelector("#checkbox");
+const body = document.querySelector("body");
+const modal = document.querySelector(".modal");
 
-button.addEventListener('change', (e) => {
+button.addEventListener("change", (event) => {
+  if (event.target.checked) {
+    body.classList.add("alarm");
+    modal.style.display = "initial";
+  } else {
+    body.classList.remove("alarm");
+    modal.style.display = "none";
+  }
+
   fetch(`/start-alarm`, {
-    method: 'post',
+    method: "post",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       alarm: e.target.checked,
