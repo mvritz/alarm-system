22a31bef9586a1fca70f09b46b3c0e42736edca0
Moritz Dresch <moritz.dresch@titanom.com>
2023-04-10 11:20:26 +0200

    fix: design

--- server/secure/alarm.js
+++ server/secure/alarm.js
@@ -1,34 +1,23 @@
-const button = document.querySelector('#checkbox');
-const body = document.querySelector('body');
+const button = document.querySelector("#checkbox");
+const body = document.querySelector("body");
+const modal = document.querySelector(".modal");
 
-let timeoutId;
-button.addEventListener('change', (event) => {
-  console.log(event);
-  if (window.confirm('Are you sure?')) {
-    console.log(event);
-    if (event.target.checked) {
-      body.classList.add('alarm');
-      event.target.disabled = true;
-      timeoutId = setTimeout(() => {
-        event.target.checked = false;
-        event.target.disabled = false;
-      }, 8000);
-    } else {
-      clearTimeout(timeoutId);
-      body.classList.remove('alarm');
-      modal.style.display = 'none';
-    }
-
-    fetch(`/start-alarm`, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        alarm: event.target.checked,
-      }),
-    });
+button.addEventListener("change", (event) => {
+  if (event.target.checked) {
+    body.classList.add("alarm");
+    // modal.style.display = "initial";
+    modal.style.display = "none";
   } else {
-    event.target.checked = false;
+    body.classList.remove("alarm");
   }
+
+  //   fetch(`/start-alarm`, {
+  //     method: "post",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       alarm: event.target.checked,
+  //     }),
+  //   });
 });
