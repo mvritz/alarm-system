cf13963c91fc32570f937d659f4f21f78415e9a8
julian-wasmeier-titanom <julian.wasmeier@titanom.com>
2023-04-10 16:40:46 +0200

    fix: readd socket.io

--- server/secure/alarm.js
+++ server/secure/alarm.js
@@ -1,74 +1,86 @@
-const button = document.querySelector("#checkbox");
-const body = document.querySelector("body");
-const modal = document.querySelector(".modal");
-const cancelBtn = document.querySelector(".cancel-btn");
-const okBtn = document.querySelector(".ok-btn");
-const timer = document.querySelector(".timer");
+const button = document.querySelector('#checkbox');
+const body = document.querySelector('body');
+const modal = document.querySelector('.modal');
+const cancelBtn = document.querySelector('.cancel-btn');
+const okBtn = document.querySelector('.ok-btn');
+const timer = document.querySelector('.timer');
 
-const minutesEl = document.getElementById("minutes");
-const secondsEl = document.getElementById("seconds");
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
 
-timer.style.display = "none";
 let minutes = 0;
 let seconds = 0;
+
+const socket = io();
+
+window.onload = () => {
+  button.disabled = true;
+  timer.style.display = 'none';
+};
 
 function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 function showModal() {
-  modal.style.display = "initial";
+  modal.style.display = 'initial';
 }
 
 function hideModal() {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 }
 
-cancelBtn.addEventListener("click", () => {
+cancelBtn.addEventListener('click', () => {
   hideModal();
   button.checked = false;
 });
 
-okBtn.addEventListener("click", (event) => {
+okBtn.addEventListener('click', (event) => {
   hideModal();
-  delay(1000).then(() => {
-    button.checked = true;
+  fetch(`/start-alarm`, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      alarm: true,
+    }),
   });
-  //   fetch(`/start-alarm`, {
-  //     method: "post",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       alarm: event.target.checked,
-  //     }),
-  //   });
-  timer.style.display = "initial";
 });
 
-button.addEventListener("change", (event) => {
+button.addEventListener('change', (event) => {
   if (event.target.checked) {
     showModal();
-  } else {
-    button.checked = false;
-    hideModal();
-    timer.style.display = "none";
-    minutes = 0;
-    seconds = 0;
   }
+  button.checked = false;
 });
 
-setInterval(() => {
-  seconds++;
+function applyRunningStyles() {
+  button.checked = true;
+  button.disabled = true;
+  body.classList.add('alarm');
+  // timer.style.display = 'initial';
+}
 
-  if (seconds === 60) {
-    seconds = 0;
-    minutes++;
-  }
+function applyStoppedStyles() {
+  button.checked = false;
+  button.disabled = false;
+  body.classList.remove('alarm');
+  hideModal();
+  timer.style.display = 'none';
+  minutes = 0;
+  seconds = 0;
+}
 
-  const formattedMinutes = minutes.toString().padStart(2, "0");
-  const formattedSeconds = seconds.toString().padStart(2, "0");
+socket.on('init', ({ isAlarm }) => {
+  isAlarm ? applyRunningStyles() : applyStoppedStyles();
+});
 
-  minutesEl.textContent = formattedMinutes;
-  secondsEl.textContent = formattedSeconds;
-}, 1000);
+socket.on('turnedOn', () => {
+  applyRunningStyles();
+});
+
+socket.on('turnedOff', () => {
+  console.log('off');
+  applyStoppedStyles();
+});
